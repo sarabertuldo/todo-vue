a01210524f598124a470ed8c2b92fb7d8742b8e7
Sara Bertuldo <sarabertuldo@gmail.com>
2021-04-26 11:33:34 -0500

    adds scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./images/charlie.jpg">
+    <TodoList msg="Sara's Vue Todo App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
